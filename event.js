137d78126cf146878b9f4f33b9e5ff7085edb630
ahnaf-official1 <ahnafzareef2007@gmail.com>
2021-08-19 16:01:31 +0600

    first commit

---
+++ event.js
@@ -0,0 +1,13 @@
+function makeBlue (){
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+const blueButton = document.getElementById('make-blue-button');
+// just set the name of the function
+blueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+            
